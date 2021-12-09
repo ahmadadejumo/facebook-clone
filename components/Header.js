eb38446550cacb6273d30a7a03533f356c2f56a2
ahmadadejumo <adejumoahmad@gmail.com>
2021-12-09 13:56:59 +0100

    imported my icons into the the header section comp

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,36 @@
+import Image from "next/image";
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from "@heroicons/react/solid";
+
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from "@heroicons/react/outline";
+
 const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <div>
+      <div>
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div>
+          <input type="text" placeholder="Search Facebook" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
